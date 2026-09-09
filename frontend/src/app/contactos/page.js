3de64414befe90c1be315f5b5337cdc996c97460
mallievi-pio <mallievi@pioix.edu.ar>
2026-09-09 09:37:44 -0300

    Cambios menores

--- frontend/src/app/contactos/page.js
+++ frontend/src/app/contactos/page.js
@@ -1,3 +1,14 @@
 export default function ListaContactos() {
-    
+    return (
+        <main>
+            {/* <ul>
+                {contactos.map((msg, index) => (
+                    <li key={`${msg.room}-${index}`}>
+                        {msg.message}
+                    </li>
+                ))}
+            </ul> */}
+        </main>
+
+    )
 }
