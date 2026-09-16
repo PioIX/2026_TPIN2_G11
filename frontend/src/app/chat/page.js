7f7fd3c8eedd0f6ee3f4d8fff82044a116072c33
mallievi-pio <mallievi@pioix.edu.ar>
2026-09-16 08:00:22 -0300

    Agregué return a chat y contactos

--- frontend/src/app/chat/page.js
+++ frontend/src/app/chat/page.js
@@ -1,3 +1,7 @@
 export default function Chat() {
-    
+    return (
+        <main>
+            
+        </main>
+    )
 }
--- frontend/src/app/contactos/page.js
+++ frontend/src/app/contactos/page.js
@@ -1,3 +1,7 @@
 export default function Contactos() {
-    
+    return (
+        <main>
+
+        </main>
+    )
 }
