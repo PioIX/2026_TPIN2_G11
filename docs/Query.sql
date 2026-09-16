f14885cece67fa0049867b986f90e99df8d0aea5
tibarzabal-web <tibarzabal@pioix.edu.ar>
2026-09-16 08:33:38 -0300

    Agregué mail a Usuarios_TP2 y actualicé el DER

--- docs/Query.sql
+++ docs/Query.sql
@@ -3,7 +3,8 @@ CREATE TABLE Usuarios_TP2 (
     nombre VARCHAR(40),
 	descripcion VARCHAR(100),
 	foto VARCHAR(255),
-    contraseña VARCHAR(20)
+    contraseña VARCHAR(20),
+    mail VARCHAR(40)
 );
 CREATE TABLE UsuariosChat_TP2 (
 	id_usChat INT auto_increment PRIMARY KEY,
@@ -36,17 +37,19 @@ CREATE TABLE Mensajes_TP2 (
     REFERENCES Chats_TP2(id_chat)
 );
 
-INSERT INTO Usuarios_TP2(nombre, descripcion, foto, contraseña)
-VALUES ("Jozusito", "Soy un vaitisito", "https://static.wikia.nocookie.net/clash-royale-esp/images/8/8e/MiniPEKKACard.webp/revision/latest/thumbnail/width/360/height/360?cb=20221210192528&path-prefix=es", "SoyUnVaitis123");
-INSERT INTO Usuarios_TP2(nombre, descripcion, foto, contraseña)
-VALUES ("Mechi", "Hola, me gustan los componentes", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFJlph8ZckCK8OrHkvuQ0s7WLez79DFu7henUaUsiOSyjJ-Yi5LRdjsoI&s=10", "holis_:)");
+INSERT INTO Usuarios_TP2(nombre, descripcion, foto, contraseña, mail)
+VALUES ("Jozusito", "Soy un vaitisito", "https://static.wikia.nocookie.net/clash-royale-esp/images/8/8e/MiniPEKKACard.webp/revision/latest/thumbnail/width/360/height/360?cb=20221210192528&path-prefix=es", "SoyUnVaitis123", "fjozuvaitis@pioix.edu.ar");
+INSERT INTO Usuarios_TP2(nombre, descripcion, foto, contraseña, mail)
+VALUES ("Mechi", "Hola, me gustan los componentes", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFJlph8ZckCK8OrHkvuQ0s7WLez79DFu7henUaUsiOSyjJ-Yi5LRdjsoI&s=10", "holis_:)", "liriofeliz@jiji.edu.ar");
 
 INSERT INTO Chats_TP2 (nombre, foto)
 VALUES ("Cumple de Vaitis", "https://static.wikia.nocookie.net/featteca/images/8/80/Jozu.png/revision/latest?cb=20220810162134&path-prefix=es");
 
 INSERT INTO Mensajes_TP2 (contenido, imagen, horario, id_usuario, id_chat)
-VALUES ("Adios", "https://static.wikia.nocookie.net/omniversal-battlefield/images/0/0c/E09D12B0-AC08-40A4-B3AE-3F4A72676FD2.jpeg/revision/latest/scale-to-width-down/537?cb=20190211200828", "2026-08-26 22:57:12",3,1);
+VALUES ("Adiós", "https://static.wikia.nocookie.net/omniversal-battlefield/images/0/0c/E09D12B0-AC08-40A4-B3AE-3F4A72676FD2.jpeg/revision/latest/scale-to-width-down/537?cb=20190211200828", "2026-08-28 14:28:33",4,1);
 
 INSERT INTO UsuariosChat_TP2 (id_usuario, id_chat)
-VALUES (3, 1);
+VALUES (5, 1);
+
+SELECT * FROM UsuariosChat_TP2;
 
