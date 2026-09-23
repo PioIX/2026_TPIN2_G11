031e620bf4b0ee0f053df54e0e7cdcecfdfa7256
mallievi-pio <mallievi@pioix.edu.ar>
2026-09-23 08:40:12 -0300

    Creé la página chat y para llegar a ella agregué el componente Link a ChatItem

---
+++ frontend/src/app/chat/page.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react"
+import Message from "./Message"
+
+export default function Chat() {
+    
+
+    return (
+
+    )
+}
--- frontend/src/components/ChatItem.js
+++ frontend/src/components/ChatItem.js
@@ -1,12 +1,14 @@
+import Link from 'next/link'
+
 export default function ChatItem({ nombre, foto }) {
   const foto_default = "/foto_default.png";
 
   const foto_chat = foto || foto_default;
 
   return (
-    <li>
+    <li onClick={router.push()}>
       <img src={foto_chat} alt={nombre} />
-      <span>{nombre}</span>
+      <Link href="/chat">{nombre}</Link>
     </li>
   );
 }
